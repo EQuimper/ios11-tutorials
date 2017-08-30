0aef9e799da57adeadb89e98b01afbd398cf7dda
EQuimper <quimperemanuel@gmail.com>
2017-08-30 16:23:48 -0400

    Design for the rear side of the menu

--- Smack/Smack/Controller/ChannelVC.swift
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -11,9 +11,11 @@ import UIKit
 class ChannelVC: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
     }
 
 }
